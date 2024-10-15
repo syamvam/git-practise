716cc94571035200e2cc21a79646fe44783e2686
syamkumary <syamyala7@gmail.com>
2024-10-15 20:20:47 +0530

    script changed

--- 11-installation.sh
+++ 11-installation.sh
@@ -8,4 +8,16 @@ then
    echo "please run this script with root priviliges"
    exit 1
 fi
-dnf install git -y   
+dnf install git -y  
+
+if [ $? -ne 0]
+   echo "Git is not installed going to install it"
+   dnf install git -y
+else
+   echo "git is already installed nothing to do"
+fi   
+
+
+
+
+
