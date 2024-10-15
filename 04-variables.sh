38a40dc9e47fce4f0422ffc253296fecf73d3ecc
syamkumary <syamyala7@gmail.com>
2024-10-15 09:39:09 +0530

    script changed

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,6 +3,4 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1:: Hi Suresh, How are you?"
-echo "Suresh:: Hello $PERSON1. I am fine. How are you doing?"
-echo "$PERSON1:: I am doing good Suresh . whats going on?"
-echo "Suresh:: I started learing shell script $PERSON1"
+echo "$PERSON2:: Hello $PERSON1. I am fine. How are you doing?"
