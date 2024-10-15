af5c11b83445d27f0385315b0377c628153b9f62
syamkumary <syamyala7@gmail.com>
2024-10-15 19:30:14 +0530

    script changed

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -5,3 +5,6 @@ echo "Number of vaiables passed $#"
 echo "Scipt name $0"
 echo "current working directory $PWD"
 echo "home directory of current user $HOME"
+echo "pid of script executing now : $$"
+sleep 100 $
+echo "pid of last background command $!"
