3e6c84d3ec2bb31dad907965020abbb1c80d096c
syamkumary <syamyala7@gmail.com>
2024-10-15 19:27:20 +0530

    script changed

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -2,3 +2,6 @@
 
 echo "all variables passed to the script $@"
 echo "Number of vaiables passed $#"
+echo "Scipt name $0"
+echo "current working directory $PWD"
+echo "home directory of current user $HOME"
