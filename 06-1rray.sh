f5c5569d764ebbe5ac6ce790fef9d58837a33ff8
syamkumary <syamyala7@gmail.com>
2024-10-15 10:05:48 +0530

    script changed

---
+++ 06-1rray.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#index starts from 0, size is 3
+FRUITS=("APPLE","KIWI","ORANGE")
+
+echo "FIRST FRUIT IS ${FRUITS[0]}"
+echo "FIRST FRUIT IS ${FRUITS[1]}"
+echo "FIRST FRUIT IS ${FRUITS[2]}"
+
+
+echo "FIRST FRUIT IS :${FRUITS[@]}"
