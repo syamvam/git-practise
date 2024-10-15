64bb1b78b6733c6167db25201040374d6d5a5108
syamkumary <syamyala7@gmail.com>
2024-10-15 20:31:58 +0530

    script changed

--- 11-installation.sh
+++ 11-installation.sh
@@ -3,17 +3,20 @@
 USERID=$(id -u)
 #echo "user id is : $USERID"
 
-if [ $USERID -ne 0 ]
-then 
-   echo "please run this script with root priviliges"
-   exit 1
-fi
+#if [ $USERID -ne 0 ]
+#then 
+ #  echo "please run this script with root priviliges"
+ #  exit 1
+#fi
 
 dnf list installed git 
 
 if [ $? -ne 0 ]
    echo "Git is not installed going to install it"
    dnf install git -y
+   if [ $? -ne 0 ]
+   then 
+      echo "git installation is not success check it"
 else
    echo "git is already installed nothing to do"
 fi   
